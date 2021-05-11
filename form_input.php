8f4054b5257475fc52b19578b6dfa6af775ce9cd
rendzz <52620414+rendzz@users.noreply.github.com>
2021-05-11 23:11:06 +0630

    Add files via upload

---
+++ form_input.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab7 by: Rendy Adi Prassetiyo</title>
+</head>
+<body style="">
+    <h1>Lab7 by: Rendy Adi Prassetiyo </h1>
+    <form style="" action="database.php" method="post">
+        Nama: 
+        <input type="text" name="nama" id="nama">
+        <br>
+        <br>
+        tanggal lahir: 
+        <input type="date" name="tgl_lahir" id="tgl_lahir">
+        <br>
+        <br>
+        Pekerjaan: 
+        <select name="pekerjaan" id="pekerjaan">
+            <option value="it_support">IT Support</option>
+            <option value="accounting">Accounting</option>
+        </select>
+        <br>
+        <br>
+        <input type="submit" value="submit" name="submit">
+    </form>
+</body>
+</html>
